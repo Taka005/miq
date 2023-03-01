f013bcfb67b645e1fed4c78580311aec5a300d20
Taka005 <01taka005@gmail.com>
2023-03-01 19:06:17 +0900

    修正

--- main.py
+++ main.py
@@ -116,52 +116,15 @@ def color_gen(name, tag, id, content, icon):
 
     tx = ImageDraw.Draw(img)
 
-    tsize_t = draw_text(img, (890, 270), content, size=45, color=(
-        255, 255, 255, 255), split_len=16, auto_expand=True)
+    tsize_t = draw_text(img, (890, 270), content, size=45, color=(255, 255, 255, 255), split_len=16, auto_expand=True)
 
     name_y = tsize_t[2] + 40
-    tsize_name = draw_text(img, (890, name_y), f"{name}#{tag}", size=25, color=(
-        255, 255, 255, 255), split_len=25, disable_dot_wrap=True)
+    tsize_name = draw_text(img, (890, name_y), f"{name}#{tag}", size=25, color=(255, 255, 255, 255), split_len=25, disable_dot_wrap=True)
 
     id_y = name_y + tsize_name[1] + 4
-    tsize_id = draw_text(img, (890, id_y), id, size=18, color=(
-        180, 180, 180, 255), split_len=45, disable_dot_wrap=True)
+    tsize_id = draw_text(img, (890, id_y), id, size=18, color=(180, 180, 180, 255), split_len=45, disable_dot_wrap=True)
 
-    tx.text((1125, 694), branding,
-            font=MPLUS_FONT, fill=(120, 120, 120, 255))
-
-    file = io.BytesIO()
-    img.save(file, format="PNG", quality=95)
-    file.seek(0)
-    return file
-
-# 反転
-def reverse_gen(name, tag, id, content, icon):
-    img = BASE_IMAGE.copy()
-
-    icon = Image.open(io.BytesIO(requests.get(icon).content))
-    icon = icon.resize((720, 720), Image.LANCZOS)
-    icon = icon.convert("L")
-    icon_filtered = ImageEnhance.Brightness(icon)
-
-    img.paste(icon_filtered.enhance(0.7), (570, 0))
-    img.paste(BASE_FLIPPED_IMAGE, (0, 0), BASE_FLIPPED_IMAGE)
-
-    tx = ImageDraw.Draw(img)
-
-    tsize_t = draw_text(img, (390, 270), content, size=45, color=(
-        255, 255, 255, 255), split_len=16, auto_expand=True)
-
-    name_y = tsize_t[2] + 40
-    tsize_name = draw_text(img, (390, name_y), f"{name}#{tag}", size=25, color=(
-        255, 255, 255, 255), split_len=25, disable_dot_wrap=True)
-
-    id_y = name_y + tsize_name[1] + 4
-    tsize_id = draw_text(img, (390, id_y), id, size=18, color=(
-        180, 180, 180, 255), split_len=45, disable_dot_wrap=True)
-
-    tx.text((15, 694), branding,
-            font=MPLUS_FONT, fill=(120, 120, 120, 255))
+    tx.text((1125, 694), "TakasumBOT#7189",font=MPLUS_FONT, fill=(120, 120, 120, 255))
 
     file = io.BytesIO()
     img.save(file, format="PNG", quality=95)
@@ -184,17 +147,6 @@ def normal():
 @app.route("/color", methods=["GET"])
 def color():
     res = color_gen(
-        request.args.get("name") or "名無し",
-        request.args.get("tag") or "0000",
-        request.args.get("id") or "0000000000000000000",
-        request.args.get("content") or "これはテストです",
-        request.args.get("icon") or "https://cdn.discordapp.com/embed/avatars/0.png"
-    )
-    return send_file(res, mimetype="image/png")
-
-@app.route("/reverse", methods=["GET"])
-def reverse():
-    res = reverse_gen(
         request.args.get("name") or "名無し",
         request.args.get("tag") or "0000",
         request.args.get("id") or "0000000000000000000",
